3f677d84cb4a7f9c44ca8e6ef26ba04d0c4af43a
Malza0408 <parkjh04080@gmail.com>
2021-11-22 23:03:51 +0900

    custom hook

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import ControlledComponent from "./components/controlledComponent";
 import UncontrolledComponent from "./components/uncontrolledComponent";
+import useWindowWidth from "./hooks/useWindowWidth";
+import { useEffect } from "react";
 
 function App() {
+  const customHookWidth = useWindowWidth();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* controlled and uncontrolled example
         <ControlledComponent />
-        <UncontrolledComponent />
+        <UncontrolledComponent /> */}
+        {/* CustomHook Example */}
+        {customHookWidth}
       </header>
     </div>
   );
